9acfce354c959cb6c98f6d6ebfc51c72679f1d26
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-13 02:27:20 +0530

    Add files via upload

---
+++ Basics/ForInLoop.js
@@ -0,0 +1,15 @@
+// for in for index 
+const fun = () => {//strnig
+    const arr = ['r' ,'g', 'b'];
+    for (let a in arr){
+        console.log(a);
+    }
+}
+fun();
+const funn = () => {//int
+    const array = [1 ,2, 3];
+    for (let a in array){
+        console.log(a);
+    }
+}
+funn();
