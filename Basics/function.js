f0ca3675d938a71734ac6169d81a58ae63c30008
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-13 02:28:40 +0530

    Add files via upload

---
+++ Basics/function.js
@@ -0,0 +1,10 @@
+function getName(param) {//definition of function
+    console.log('hello world');
+    
+}
+getName();//calling function
+const getNames = () => {//arrow method
+    console.log('arrow method');
+    
+}
+getNames();//calling function
