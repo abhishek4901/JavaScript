4b63a926931fa55439ccf64a8d4a501de2ea0c6e
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-02 22:51:15 +0530

    Update ObjectCreate.js

--- Basics/ObjectCreate.js
+++ Basics/ObjectCreate.js
@@ -1,11 +1,14 @@
 // Creating an object using object literal
 let person = {
-    name: "John",
+    name: "raj",
     age: 25,
     city: "Delhi"
 };
 
-// Accessing object properties
-console.log("Name:", person.name);
-console.log("Age:", person.age);
-console.log("City:", person.city);
+// Accessing object  
+console.log(person);//{ name: 'raj', age: 25, city: 'Delhi' }
+// Accessing object  keys
+console.log("Name:", person.name);//raj
+console.log("Age:", person.age);//25
+console.log(person["name"]);//raj
+console.log(person["age"]);//25
