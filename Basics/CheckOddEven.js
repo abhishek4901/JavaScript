67b52799235c8f2d82e2083919c6a945a0a55ae6
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-03 03:00:28 +0530

    CheckOddEven.js

--- Basics/CheckOddEven.js
+++ Basics/CheckOddEven.js
@@ -5,7 +5,7 @@ console.log("You entered:", n);
  function checkEvenodd (n) {
     if (n%2==0)
        {
-         console.log('number is even')
+         console.log('number is even') 
        } 
     else  
     console.log('number is odd') 
