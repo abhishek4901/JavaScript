2617220d6ab9cdcf2d9df6b0fde983a4f02abaec
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-07 07:22:29 +0530

    .

---
+++ Programs/PowerOff.js
@@ -0,0 +1,10 @@
+function getpower(a,b){
+    console.log(a**b);
+}
+getpower(5,2);
+
+const getpower2=(a,b) =>{
+    console.log(a**b)
+}
+
+getpower2(2,10)
---
+++ Programs/SumofTwonum.js
@@ -0,0 +1,13 @@
+
+
+var poowerof = (a,b ) => {//pass the argument
+    console.log (a**b);
+}
+
+poowerof(4,2);//pass the actual argument
+
+let sum = (a,b ) => {//pass the argument
+    console.log (+b);
+}
+
+sum(4,2);//pass the actual argument    
