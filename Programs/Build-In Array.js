39029185e07a9a71e91e3d57ebad6daeabb093fa
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-17 21:32:18 +0530

    Build-In Array.js

--- Programs/Build-In Array.js
+++ Programs/Build-In Array.js
@@ -5,7 +5,7 @@ let numbers = [10, 20, 30, 40, 50];
 // 1. forEach: Print each element
 numbers.forEach(function(num) { 
     console.log("Number:", num);  
-});   
+});    
  
 // 2. map: Create a new array with each value doubled
 let doubled = numbers.map(function(num) {
