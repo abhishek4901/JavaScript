482ee1e32ce08885fa9523e004862eea127d2e0a
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-23 16:44:27 +0530

    Change BackgroundColorOnClick.js

--- Programs/Change BackgroundColorOnClick.js
+++ Programs/Change BackgroundColorOnClick.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Function to generate a random color
     function getRandomColor() {
-        const letters = '0123456789ABCDEF'; 
+        const letters = '0123456789ABCDEF';  
         let color = '#';
         for (let i = 0; i < 6; i++) { 
             color += letters[Math.floor(Math.random() * 16)];
